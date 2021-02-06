6db860a4d6a832470c71ac453e886f12c2ade326
Kai Devrim <kkgit@devrim.tech>
2021-02-06 00:27:23 -0800

    More changes to the site, good base with TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: ["./src/**/*.html", "./src/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["polt"],
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: { padding: ["hover"] },
   },
   plugins: [],
 };
